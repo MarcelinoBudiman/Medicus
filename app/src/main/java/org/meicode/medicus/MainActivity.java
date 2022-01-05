dc61904779739fb8709f7766eb96d01e95a9b95a
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-05 19:51:01 +0700

    Add Splash Screen

--- app/src/main/java/org/meicode/medicus/MainActivity.java
+++ app/src/main/java/org/meicode/medicus/MainActivity.java
@@ -3,12 +3,32 @@ package org.meicode.medicus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnimation;
+    Animation bottomAnimation;
+    ImageView logoImg;
+    TextView logoTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logoImg = findViewById(R.id.logoImg);
+        logoTxt = findViewById(R.id.logoTxt);
+
+        logoImg.setAnimation(topAnimation);
+        logoTxt.setAnimation(bottomAnimation);
     }
 }
