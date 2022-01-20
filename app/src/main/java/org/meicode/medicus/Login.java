ec54027649d1c408b5d1d507288526a98f6cdf12
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-20 18:09:36 +0700

    Fixed Login with Huawei API

--- app/src/main/java/org/meicode/medicus/Login.java
+++ app/src/main/java/org/meicode/medicus/Login.java
@@ -3,12 +3,112 @@ package org.meicode.medicus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
+import android.util.Log;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+import com.huawei.hms.support.api.entity.common.CommonConstant;
 
 public class Login extends AppCompatActivity {
+
+    // AccountAuthService provides a set of APIs, including silentSignIn, getSignInIntent, and signOut.
+    private AccountAuthService mAuthService;
+    // Set HUAWEI ID sign-in authorization parameters.
+    private AccountAuthParams mAuthParam;
+    // Define the request code for signInIntent.
+    private static final int REQUEST_CODE_SIGN_IN = 1000;
+    // Define the log tag.
+    private static final String TAG = "Account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                silentSignInByHwId();
+            }
+        });
+
     }
+
+    private void silentSignInByHwId() {
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setEmail()
+                .createParams();
+
+        // Use AccountAuthParams to build AccountAuthService.
+        mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+        // Use AccountAuthParams to build AccountAuthService.
+        mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+        // Use silent sign-in to sign in with a HUAWEI ID.
+        Task<AuthAccount> task = mAuthService.silentSignIn();
+        task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+            @Override
+            public void onSuccess(AuthAccount authAccount) {
+                // The silent sign-in is successful. Process the returned account object AuthAccount to obtain the HUAWEI ID information.
+                dealWithResultOfSignIn(authAccount);
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                // The silent sign-in fails. Use the getSignInIntent() method to show the authorization or sign-in screen.
+                if (e instanceof ApiException) {
+                    ApiException apiException = (ApiException) e;
+                    Intent signInIntent = mAuthService.getSignInIntent();
+                    // If your app appears in full-screen mode duing user sign-in, that is, with no satus bar at the top of the phone screen, add the following parameter in the intent:
+                    // intent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                    // Check the details in this FAQ.
+                    signInIntent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                    startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+                }
+            }
+        });
+
+    }
+
+    private void dealWithResultOfSignIn(AuthAccount authAccount) {
+
+        Log.i(TAG, "display name:" + authAccount.getDisplayName());
+        Log.i(TAG, "photo uri string:" + authAccount.getAvatarUriString());
+        Log.i(TAG, "photo uri:" + authAccount.getAvatarUri());
+        Log.i(TAG, "email:" + authAccount.getEmail());
+        Log.i(TAG, "openid:" + authAccount.getOpenId());
+        Log.i(TAG, "unionid:" + authAccount.getUnionId());
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            if (authAccountTask.isSuccessful()) {
+                // The sign-in is successful, and the authAccount object that contains the HUAWEI ID information is obtained.
+                AuthAccount authAccount = authAccountTask.getResult();
+                dealWithResultOfSignIn(authAccount);
+                Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
+            } else {
+                // The sign-in fails. Find the failure cause from the status code. For more information, please refer to Error Codes.
+                Log.e(TAG, "sign in failed : " +((ApiException)authAccountTask.getException()).getStatusCode());
+            }
+        }
+    }
+
 }
