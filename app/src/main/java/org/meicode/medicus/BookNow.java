312c0d0bc4dc6cce163d95a8f5a8e4175109fb3d
Meruki888 <97108814+Meruki888@users.noreply.github.com>
2022-01-20 22:14:17 +0700

    Membuat halaman book_history

---
+++ app/src/main/java/org/meicode/medicus/BookNow.java
@@ -0,0 +1,14 @@
+package org.meicode.medicus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookNow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_now);
+    }
+}
