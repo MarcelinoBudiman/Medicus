1dad1aae7529ad65f08f91098cf2a3b4561a028b
MarcelinoBudiman <marcelinobudiman47@gmail.com>
2022-01-21 17:28:46 +0700

    add adapter for BookHistory

--- app/src/main/java/org/meicode/medicus/BookingHistory.java
+++ app/src/main/java/org/meicode/medicus/BookingHistory.java
@@ -1,19 +1,29 @@
 package org.meicode.medicus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BookingHistory extends AppCompatActivity {
 
     RecyclerView recycler;
+    String doctorName[];
+    String hospital[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_history);
 
-        recycler = findViewById(R.id.bookHistory);
+        recycler = findViewById(R.id.bookHistoryRecycler);
+        Intent i;
+
+        HistoryAdapter adapter = new HistoryAdapter(this, doctorName, hospital);
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/org/meicode/medicus/HistoryAdapter.java
+++ app/src/main/java/org/meicode/medicus/HistoryAdapter.java
@@ -1,40 +1,60 @@
 package org.meicode.medicus;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.MyViewHolder> {
 
     Context c;
+    String doctorName[];
+    String hospital[];
 
-    public HistoryAdapter(Context context,){
+    public HistoryAdapter(Context context, String name[], String h[]){
+
         c = context;
+        doctorName = name;
+        hospital = h;
     }
 
     @NonNull
     @Override
-    public HistoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(c);
+        View v = inflater.inflate(R.layout.booking_history_row, parent, false);
+        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HistoryAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull HistoryAdapter.MyViewHolder holder, int position) {
+        holder.date.setText();
+        holder.lblDoctor.setText("Appointment With:");
+        holder.doctorName.setText(doctorName[position]);
+        holder.lblHospital.setText("Appointment At:");
+        holder.hospital.setText(hospital[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return doctorName.length;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        public ViewHolder(@NonNull View itemView) {
+        TextView date, lblDoctor, doctorName, lblHospital, hospital;
+
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            date = itemView.findViewById(R.id.lblDate);
+            lblDoctor = itemView.findViewById(R.id.lblDoctorName);
+            doctorName = itemView.findViewById(R.id.insDoctorName);
+            lblHospital = itemView.findViewById(R.id.insDoctorName);
+            hospital = itemView.findViewById(R.id.insHospital);
         }
     }
 }
